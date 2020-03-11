54acfb1b223dd8a0b77364eacf86fd64b879b77e
James Manahan <manahanjames@yahoo.com>
2020-03-11 12:15:54 -0700

    Create Apollo Server Instance

--- server/server.js
+++ server/server.js
@@ -7,6 +7,10 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+//create ApolloServer instance, plug in Apollo into existing Express app
+const apolloServer = new ApolloServer({typeDefs, resolvers});
+apolloServer.applyMiddleware({app, path: '/graphql'});
+
 app.get('/api/hello', (req, res) => {
     res.send({ express: 'Hello From Express' });
 });
