7488ecfb9beeb3892592d6b82ee98b2d90f119fa
James Manahan <manahanjames@yahoo.com>
2020-03-13 22:20:03 -0700

    Added Comments

--- server/resolvers.js
+++ server/resolvers.js
@@ -1,6 +1,7 @@
 //import database information
 const db = require('./db');
-
+//must mirror type definitions exactly
+//schema declares the type, resolvers returns actual value
 const Query = {
     greeting: (parent, args, ctx, info) => 'Hello World!'
 };
