2318af2499a3c77fa92bda1ccba5ce8737e1ce03
James Manahan <manahanjames@yahoo.com>
2020-03-14 22:27:44 -0700

    Comments

--- server/resolvers.js
+++ server/resolvers.js
@@ -2,6 +2,7 @@
 const db = require('./db');
 //must mirror type definitions exactly
 //schema declares the type, resolvers returns actual value
+//always a post request: client posts json, server responds with json
 const Query = {
     greeting: (parent, args, ctx, info) => 'Hello World!'
 };
