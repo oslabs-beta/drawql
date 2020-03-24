6f46db65fe20feaf9015f23443dc824617eca009
Anthony Flores <anthony.flores2293@gmail.com>
2020-03-23 22:51:32 -0700

    user schema comments

--- server/schema/user.js
+++ server/schema/user.js
@@ -27,3 +27,11 @@ export default gql`
 
 // Belongs to type User
 // organizaiton_name: String
+
+// The properties of the type Query allow for multiple, single or a specific user to be searched for in the conected database
+
+// the signup and login properties of the type mutation allow a user to create a profile, and later login to that profile
+
+// the type token is produced on every sign up and every login so that the user can be authenticated for every newly created drawing, and for every deletion of a drawing
+
+// the type user sets the strucuture of the required data to create a user.
