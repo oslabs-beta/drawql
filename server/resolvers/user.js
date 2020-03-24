d6acd7d1421899b405911e91f189a9ac3c01493e
Jessikee Campbell-Walker <jessikeecw@gmail.com>
2020-03-23 22:56:56 -0700

    Added a create token func that secures passwords

--- server/resolvers/user.js
+++ server/resolvers/user.js
@@ -1,4 +1,12 @@
 import jwt from 'jsonwebtoken';
 import { AuthenticatedError, UserInputError } from 'apollo-server';
 
-const createToken =async(user,secret,expiresIn)=>{}
+//creates a decoded token
+const createToken = async (user, secret, expiresIn) => {
+    const { id, email, username } = user;
+    return await jwt.sign({ id, email, username }, secret, {
+        expiresIn
+    });
+};
+export default {
+}
