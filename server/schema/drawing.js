41812dba5a8eb5a4ed8931f12ab32998f92a11a4
Anthony Flores <anthony.flores2293@gmail.com>
2020-03-23 22:57:28 -0700

    drawing schema comments

--- server/schema/drawing.js
+++ server/schema/drawing.js
@@ -28,3 +28,9 @@ export default gql`
 
 // Belongs to type Drawing
 // organization_id: Organization
+
+// the two properties inside of the query type allow for all of the drawings to be retrieved from the database, or for one single drawing based off the drawings specific ID
+
+// the type mutation has two main properties which allow a drawing to be created and deleted as long as the user has been authenticated
+
+// the type drawing displays the required structure of data needed in order to create a new drawing.
