3f2e52c0ec985d8cfb2aca5532df77e654e625ee
Jessikee Campbell-Walker <jessikeecw@gmail.com>
2020-03-23 23:30:03 -0700

    created mutations for signing up user and loggin in user

--- server/resolvers/user.js
+++ server/resolvers/user.js
@@ -13,14 +13,37 @@ export default {
         // finds all users
         users: async (parent, args, { models }) => {
             return models.User.findAll();
-    },
-      //finds user by id if the person exist
+        },
+        //finds user by id if the person exist
         user: async (parent, args, { models, person }) => {
             if (!person) {
                 return null;
             }
             return await models.User.findByPk(person.id);
         }
-  },
-  
+    },
+    Mutation: {
+        signUp: async (
+            parent,
+            { username, email, password },
+            { models, secret }
+        ) => {
+            const user = await models.User.create({
+                username,
+                email,
+                password
+            });
+            return { token: createToken(user, secret, '30m') };
+        },
+        logIn: async (parent, { login, password }, { models, secret }) => {
+            const user = await models.User.findByLogin(login);
+
+            if (!user) {
+                throw new UserInputError(
+                    'No user found with this login credentials'
+                );
+            }
+            const isValid = await user.validatePassword(password);
+        }
+    }
 };
