6119313c5527a3437838c5d630598cecc6baef89
Jessikee Campbell-Walker <jessikeecw@gmail.com>
2020-04-08 12:26:41 -0700

    fixed merge changes

--- client/src/index.js
+++ client/src/index.js
@@ -1,20 +1,16 @@
-import React from 'react';
+import React, { forwardRef } from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
-import { ApolloProvider } from 'react-apollo';
-import { ApolloClient, InMemoryCache, HttpLink } from 'apollo-boost';
+import { ApolloClient, HttpLink, InMemoryCache } from 'apollo-boost';
+import { ApolloProvider } from '@apollo/react-hooks';
 
-// Import components...
+//Import components ...
 import Index from './views/Index';
-import Login from './views/pages/Login';
-import Register from './views/pages/Register';
+import loginPage from './views/pages/Login';
+import registerPage from './views/pages/Register';
 import testProto from './views/pages/testProto';
 import PrototypeContainer from './views/pages/PrototypeContainer';
 import App from './App';
-
-// import GQL stuff so we can create our initial state
-import resolvers from './graphql/resolvers';
-import typeDefs from './graphql/typeDefs';
 
 // import css/sass
 import './index.css';
@@ -24,33 +20,70 @@ import './assets/scss/argon-design-system-react.scss';
 
 import * as serviceWorker from './serviceWorker';
 
+//tells your network to send the cookie along with every request
 const link = new HttpLink({
-    uri: 'http://localhost:5000',
-    credentials: 'include',
-    headers: {
-        authorization: localStorage.getItem('token')
+    uri: 'http://localhost:3000/graphql',
+    //passes the credential option if the server has a different domain
+    credentials: 'include'
+});
+
+const cache = new InMemoryCache({
+    // cacheRedirects: {
+    //     Query: {
+    //         user: (_, id , { getCacheKey }) =>
+    //             getCacheKey({ __typename: 'User', id })
+    //     }
+    // }
+});
+
+//passed through the fetch
+const client = new ApolloClient({
+    link,
+    cache,
+    // typeDefs,
+    // resolvers,
+
+    //access the token from the headers
+    request: async operation => {
+        const token = localStorage.getItem('token') || null;
+        //passes token to the headers
+        operation.setContext({
+            headers: {
+                Authorization: token ? `Bearer ${token}` : ''
+            }
+        });
+    },
+    onError: ({ graphQLErrors, networkError }) => {
+        if (graphQLErrors) {
+            graphQLErrors.forEach(({ message, locations, path }) => {
+                console.log(
+                    `GraphQLerrors: message:${message},location:${locations},path:${path}`
+                );
+            });
+        }
+        if (networkError) {
+            // logoutUser();
+            console.log(`Networkerrors: ${networkError}`);
+        }
     }
 });
 
-const cache = new InMemoryCache();
-
-const client = new ApolloClient({
-    cache,
-    link,
-    typeDefs,
-    resolvers
-});
-
-// creates an initial state
 cache.writeData({
     data: {
+        user: [],
+        // drawing:[],
+        // sidebarHidden: false
+        
         //  the double-bang returns the boolean true/false association of a value, so we use it to set a truthy value rather than evaluating the value of 'token'
         // isLoggedIn can be used to conditionally display login/signup areas of navbar
         isLoggedIn: !!localStorage.getItem('token'),
         sidebarOpen: true
+    
     }
 });
 
+//connects apollo client to react
+//apolloprovider is similar to context.provider, it wraps the app andplaces the client on the context.Which gives the ability to access it anywhere in your component tree
 ReactDOM.render(
     <ApolloProvider client={client}>
         <BrowserRouter>
@@ -64,13 +97,16 @@ ReactDOM.render(
                 <Route
                     path="/login"
                     exact
-                    render={props => <Login {...props} />}
+                    // render={props => <Login {...props} />}
+                    component={loginPage}
                 />
                 <Route
                     path="/register"
                     exact
-                    render={props => <Register {...props} />}
+                    // render={props => <Register {...props} />}
+                    component={registerPage}
                 />
+                {/* I dont recall why this needs to be here? Presumably something that happens after login? */}
                 <Route path="/app" exact component={testProto} />
                 <Route path="/proto" exact component={PrototypeContainer} />
                 <Redirect to="/" />
